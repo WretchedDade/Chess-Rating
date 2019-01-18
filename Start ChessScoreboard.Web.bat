f4511c34ea3c321a4a00655280530e4baa2d0491
Dade Cook <dadecook@gmail.com>
2019-01-18 09:01:10 -0500

    Separating ChessScoreboad's Web and Command Line Interfaces

--- Start ChessScoreboard.Web.bat
+++ Start ChessScoreboard.Web.bat
@@ -1,4 +1,3 @@
-cd ChessScoreboard.Web
 cd dist
 start cmd /k node ChessScoreboard.Web.Server.js
 start "" http://localhost:8000/
--- Start ChessScoreboard.Web.dev.bat
+++ Start ChessScoreboard.Web.dev.bat
@@ -1,4 +1,3 @@
-cd ChessScoreboard.Web
 cd src
 start cmd /k node ChessScoreboard.Web.Server.js
 start "" http://localhost:8000/
